bd5df0a477e2849fd32d140a0ea781f97066d7e0
oscar jonathan misael lin coronado <ojmlc93@gmail.com>
2020-05-21 13:45:16 -0700

    [ADD] Se agregaron detalles de información a la grafica

--- data_visaulisation.py
+++ data_visaulisation.py
@@ -1,13 +1,17 @@
 from matplotlib import pyplot as plot
 from matplotlib import dates as plot_dates
 from datetime import date
-import numpy as np
 
 plot.style.use('seaborn')
 
 def show_plot_dates(dates_array, country_dates_info):
-    dates_array = np.array(dates_array)
-    country_dates_info = np.array(country_dates_info)
+    date_format = plot_dates.DateFormatter('%b, %d, %Y')
+
     plot.plot_date(dates_array, country_dates_info, linestyle='solid')
+    plot.gcf().autofmt_xdate()
+    plot.gca().xaxis.set_major_formatter(date_format)
+    plot.title('Casos confirmados de COVID-19')
+    plot.xlabel('Tiempo')
+    plot.ylabel('Numero de casos')
     plot.tight_layout()
     plot.show()
